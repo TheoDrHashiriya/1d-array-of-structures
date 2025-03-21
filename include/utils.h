89f84ec3bfdd937b36a37d3f6e106c461eb26ec6
TheoDrHashiriya <74580203+TheoDrHashiriya@users.noreply.github.com>
2025-03-21 20:16:20 +0800

    Delete include/utils.h

--- include/utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "student.h"
-
-void clearScreen();
-void pauseScreen();
-char enterOption(char option);
-std::string enterSearchID();
-void sortAlphabetically(Student students[], int studentCount);
-void sortByGPA(Student students[], int studentCount);
